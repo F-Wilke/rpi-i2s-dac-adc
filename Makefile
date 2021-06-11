2547f8110a8c4d0d77d39397da86f024ef8fb74f
F-Wilke <ich@flj-wilke.de>
2021-06-11 17:58:42 +0200

    iinitial commit

---
+++ Makefile
@@ -0,0 +1,68 @@
+MOD_NAME	:= rpi-i2s-dac-adc
+
+ifneq ($(KERNELRELEASE),)
+
+# call from kernel build system
+
+all:
+	@echo "Building from the kernel build system"
+	@echo "Module build: $(CONFIG_SND_I2S_RPI)"
+	@echo "Name: $(MOD_NAME)"
+
+obj-$(CONFIG_SND_I2S_RPI) := $(MOD_NAME).o
+
+else
+# external module build
+
+EXTRA_FLAGS += -I$(PWD)
+
+#
+# KDIR is a path to a directory containing kernel source.
+# It can be specified on the command line passed to make to enable the module to
+# be built and installed for a kernel other than the one currently running.
+# By default it is the path to the symbolic link created when
+# the current kernel's modules were installed, but
+# any valid path to the directory in which the target kernel's source is located
+# can be provided on the command line.
+#
+KDIR	?= /lib/modules/$(shell uname -r)/build
+MDIR	?= /lib/modules/$(shell uname -r)
+PWD	:= $(shell pwd)
+PWD	:= $(shell pwd)
+
+export CONFIG_SND_I2S_RPI := m
+
+all:
+	$(MAKE) -C $(KDIR) M=$(PWD) 
+	
+	rm bin/$(MOD_NAME).dtbo
+	dtc -@ -Hepapr -I dts -O dtb -o bin/$(MOD_NAME).dtbo rpi-i2s-dac-adc-overlay.dts
+
+clean:
+	$(MAKE) -C $(KDIR) M=$(PWD) clean
+
+	rm bin/$(MOD_NAME).dtbo
+	dtc -@ -Hepapr -I dts -O dtb -o bin/$(MOD_NAME).dtbo rpi-i2s-dac-adc-overlay.dts
+
+help:
+	$(MAKE) -C $(KDIR) M=$(PWD) help
+
+install: rpi-i2s-dac-adc.ko
+	rm -f ${MDIR}/kernel/sound/drivers/$(MOD_NAME).ko
+	rm /boot/overlays/$(MOD_NAME).dtbo
+
+	install -m644 -b -D $(MOD_NAME).ko ${MDIR}/kernel/sound/drivers/$(MOD_NAME).ko
+	cp bin/$(MOD_NAME).dtbo /boot/overlays/$(MOD_NAME).dtbo
+
+	depmod -aq
+	
+
+uninstall:
+	rm -rf ${MDIR}/kernel/sound/drivers/$(MOD_NAME).ko
+	rm /boot/overlays/$(MOD_NAME).dtbo
+	depmod -aq
+
+endif
+
+.PHONY : all clean install uninstall
+
---
+++ rpi-i2s-dac-adc.c
@@ -0,0 +1,179 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * rpi-simple-soundcard.c -- ALSA SoC Raspberry Pi soundcard.
+ *
+ * Copyright (C) 2018 Raspberry Pi.
+ *
+ * Authors: Tim Gover <tim.gover@raspberrypi.org>
+ *
+ * Based on code:
+ * hifiberry_amp.c, hifiberry_dac.c, rpi-dac.c
+ * by Florian Meier <florian.meier@koalo.de>
+ *
+ * googlevoicehat-soundcard.c
+ * by Peter Malkin <petermalkin@google.com>
+ *
+ * adau1977-adc.c
+ * by Andrey Grodzovsky <andrey2805@gmail.com>
+ *
+ * merus-amp.c
+ * by Ariel Muszkat <ariel.muszkat@gmail.com>
+ *		Jorgen Kragh Jakobsen <jorgen.kraghjakobsen@infineon.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/gpio/consumer.h>
+
+#include <sound/core.h>
+#include <sound/pcm.h>
+#include <sound/pcm_params.h>
+#include <sound/soc.h>
+
+/* Parameters for generic RPI functions */
+struct snd_rpi_simple_drvdata {
+	struct snd_soc_dai_link *dai;
+	const char* card_name;
+	unsigned int fixed_bclk_ratio;
+};
+
+static struct snd_soc_card snd_rpi_simple = {
+	.driver_name  = "RPi-simple",
+	.owner        = THIS_MODULE,
+	.dai_link     = NULL,
+	.num_links    = 1, /* Only a single DAI supported at the moment */
+};
+
+static int snd_rpi_simple_init(struct snd_soc_pcm_runtime *rtd)
+{
+	struct snd_rpi_simple_drvdata *drvdata =
+		snd_soc_card_get_drvdata(rtd->card);
+	struct snd_soc_dai *cpu_dai = asoc_rtd_to_cpu(rtd, 0);
+
+	if (drvdata->fixed_bclk_ratio > 0)
+		return snd_soc_dai_set_bclk_ratio(cpu_dai,
+				drvdata->fixed_bclk_ratio);
+
+	return 0;
+}
+
+static int snd_rpi_simple_hw_params(struct snd_pcm_substream *substream,
+		struct snd_pcm_hw_params *params)
+{
+	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+	struct snd_soc_dai *cpu_dai = asoc_rtd_to_cpu(rtd, 0);
+	struct snd_rpi_simple_drvdata *drvdata;
+	unsigned int sample_bits;
+
+	drvdata = snd_soc_card_get_drvdata(rtd->card);
+
+	if (drvdata->fixed_bclk_ratio > 0)
+		return 0; // BCLK is configured in .init
+
+	/* The simple drivers just set the bclk_ratio to sample_bits * 2 so
+	 * hard-code this for now. More complex drivers could just replace
+	 * the hw_params routine.
+	 */
+	sample_bits = snd_pcm_format_physical_width(params_format(params));
+	return snd_soc_dai_set_bclk_ratio(cpu_dai, sample_bits * 2);
+}
+
+static struct snd_soc_ops snd_rpi_simple_ops = {
+	.hw_params = snd_rpi_simple_hw_params,
+};
+
+SND_SOC_DAILINK_DEFS(rpi_i2s_dac_adc,
+	DAILINK_COMP_ARRAY(COMP_CPU("bcm2708-i2s.0")),
+	DAILINK_COMP_ARRAY(
+		COMP_CODEC("pcm5102a-codec", "pcm5102a-hifi"),
+		COMP_CODEC("dmic-codec", "dmic-hifi")),
+	DAILINK_COMP_ARRAY(COMP_CPU("bcm2708-i2s.0")));
+
+static struct snd_soc_dai_link snd_rpi_i2s_dac_adc_dai[] = {
+	{
+		.name           = "TeKaDe rpi_i2s_dac_adc",
+		.stream_name    = "TeKaDe rpi_i2s_dac_adc stream",
+		.dai_fmt        = SND_SOC_DAIFMT_I2S |
+					SND_SOC_DAIFMT_NB_NF |
+					SND_SOC_DAIFMT_CBS_CFS,
+		SND_SOC_DAILINK_REG(rpi_i2s_dac_adc),
+	},
+};
+
+static struct snd_rpi_simple_drvdata drvdata_rpi_i2s_dac_adc = {
+	.card_name = "snd_rpi_i2s_dac_adc",
+	.dai       = snd_rpi_i2s_dac_adc_dai,
+};
+
+static const struct of_device_id snd_rpi_i2s_dac_adc_of_match[] = {
+	{ .compatible = "tekade, rpi-i2s-dac-adc",
+		.data = (void *) &drvdata_rpi_i2s_dac_adc },
+};
+
+static int snd_rpi_i2s_dac_adc_probe(struct platform_device *pdev)
+{
+	int ret = 0;
+	const struct of_device_id *of_id;
+
+	snd_rpi_simple.dev = &pdev->dev;
+	of_id = of_match_node(snd_rpi_i2s_dac_adc_of_match, pdev->dev.of_node);
+
+	if (pdev->dev.of_node && of_id->data) {
+		struct device_node *i2s_node;
+		struct snd_rpi_simple_drvdata *drvdata =
+			(struct snd_rpi_simple_drvdata *) of_id->data;
+		struct snd_soc_dai_link *dai = drvdata->dai;
+
+		snd_soc_card_set_drvdata(&snd_rpi_simple, drvdata);
+
+		/* More complex drivers might override individual functions */
+		if (!dai->init)
+			dai->init = snd_rpi_simple_init;
+		if (!dai->ops)
+			dai->ops = &snd_rpi_simple_ops;
+
+		snd_rpi_simple.name = drvdata->card_name;
+
+		snd_rpi_simple.dai_link = dai;
+		i2s_node = of_parse_phandle(pdev->dev.of_node,
+				"i2s-controller", 0);
+		if (!i2s_node) {
+			pr_err("Failed to find i2s-controller DT node\n");
+			return -ENODEV;
+		}
+
+		dai->cpus->of_node = i2s_node;
+		dai->platforms->of_node = i2s_node;
+	}
+
+	ret = devm_snd_soc_register_card(&pdev->dev, &snd_rpi_simple);
+	if (ret && ret != -EPROBE_DEFER)
+		dev_err(&pdev->dev, "Failed to register card %d\n", ret);
+
+	return ret;
+}
+
+static struct platform_driver snd_rpi_i2s_dac_adc_driver = {
+	.driver = {
+		.name   = "rpi-i2s-dac-adc-drv",
+		.owner  = THIS_MODULE,
+		.of_match_table = snd_rpi_i2s_dac_adc_of_match,
+	},
+	.probe          = snd_rpi_i2s_dac_adc_probe,
+};
+MODULE_DEVICE_TABLE(of, snd_rpi_i2s_dac_adc_of_match);
+
+module_platform_driver(snd_rpi_i2s_dac_adc_driver);
+
+MODULE_AUTHOR("Fredrik Wilke <fw@tekade.eu>");
+MODULE_DESCRIPTION("Bidirectional i2s driver");
+MODULE_LICENSE("GPL v2");
